462ff00004062389ce8036132ce18e064f5ae4be
94Spike94 <93727593+94Spike94@users.noreply.github.com>
2024-10-17 15:10:35 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,37 +1,22 @@
 Config = {}
 
--- Koordinaten für Angelstellen
-Config.FishingSpots = {
-    {x = -1604.664, y = 5256.856, z = 3.974},
-    {x = -1601.264, y = 5261.856, z = 4.000}
-}
-
 -- Taste, um mit dem Angeln zu beginnen
 Config.FishingKey = 38 -- E Taste
 
--- Verschiedene Fische und deren Chancen (Prozentsatz)
+-- Koordinaten für Angelstellen
+Config.FishingSpots = {
+    {x = 1244.9058, y = -2718.3184, z = 1.8573}, -- Beispiel Koordinaten für Angelstellen 1244.9058, -2718.3184, 1.8573, 149.6978
+    {x = -1601.264, y = 5261.856, z = 4.000}  -- Weitere Angelspots können hier hinzugefügt werden
+}
+
+-- Verschiedene Fische und ihre Wahrscheinlichkeiten (Prozentsatz)
 Config.FishTypes = {
-    {name = "Lachs", chance = 50}, -- 50% Wahrscheinlichkeit
-    {name = "Thunfisch", chance = 30}, -- 30% Wahrscheinlichkeit
-    {name = "Hai", chance = 10}, -- 10% Wahrscheinlichkeit
-    {name = "Karpfen", chance = 10} -- 10% Wahrscheinlichkeit
+    {name = "alligator_gar", chance = 50}, -- 50% Wahrscheinlichkeit
+    {name = "common_bleak", chance = 30}, -- 30% Wahrscheinlichkeit
+    {name = "pufferfish", chance = 10}, -- 10% Wahrscheinlichkeit
+    {name = "greatwhiteshark", chance = 10} -- 10% Wahrscheinlichkeit
 }
 
--- Verkäufer Position
-Config.Seller = {x = -45.564, y = -1754.210, z = 29.421}
-
--- Preise für die verschiedenen Fische
-Config.FishPrices = {
-    ["Lachs"] = 100,
-    ["Thunfisch"] = 150,
-    ["Hai"] = 500,
-    ["Karpfen"] = 50
-}
-
--- Schwierigkeitsgrad für jeden Fisch (die Zeit, die du warten musst, um ihn zu fangen, in Sekunden)
-Config.FishDifficulty = {
-    ["Lachs"] = 5,
-    ["Thunfisch"] = 10,
-    ["Hai"] = 20,
-    ["Karpfen"] = 7
-}
+-- Die minimale und maximale Anzahl an Fischen, die der Spieler alle 5 Minuten erhalten kann
+Config.MinFishCount = 1
+Config.MaxFishCount = 5
