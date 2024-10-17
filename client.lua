b7cb5873d05a5ee75fd669fabdc31407b6fcad44
94Spike94 <93727593+94Spike94@users.noreply.github.com>
2024-10-17 15:10:52 +0200

    Update client.lua

--- client.lua
+++ client.lua
@@ -15,41 +15,32 @@ function isNearFishingSpot()
     return false, nil
 end
 
--- Startet den Angelprozess
+-- Startet den Angelprozess mit Animation
 function startFishing()
     isFishing = true
     local playerPed = PlayerPedId()
+
+    -- Spielt die Angelanimation ab
     TaskStartScenarioInPlace(playerPed, "WORLD_HUMAN_STAND_FISHING", 0, true)
+    
+    TriggerEvent('chat:addMessage', { args = { "Fishing", "Du hast angefangen zu angeln. Du erhältst alle 5 Minuten Fische." } })
 
     Citizen.CreateThread(function()
         while isFishing do
-            local fish = catchFish()
-            if fish then
-                TriggerServerEvent('fishing:catch', fish)
-                Citizen.Wait(Config.FishDifficulty[fish] * 1000) -- Wartezeit je nach Schwierigkeitsgrad des Fisches
-            end
+            -- Warte 5 Minuten (300 Sekunden)
+            Citizen.Wait(300000) 
+
+            -- Sende ein Event an den Server, um Fische zu erhalten
+            TriggerServerEvent('fishing:giveFish')
         end
     end)
 end
 
--- Stoppt den Angelprozess
+-- Stoppt den Angelprozess und beendet die Animation
 function stopFishing()
     isFishing = false
     ClearPedTasksImmediately(PlayerPedId())
-end
-
--- Fängt einen zufälligen Fisch basierend auf der Wahrscheinlichkeit
-function catchFish()
-    local randomNum = math.random(1, 100)
-    local cumulativeChance = 0
-
-    for _, fish in pairs(Config.FishTypes) do
-        cumulativeChance = cumulativeChance + fish.chance
-        if randomNum <= cumulativeChance then
-            return fish.name
-        end
-    end
-    return nil
+    TriggerEvent('chat:addMessage', { args = { "Fishing", "Du hast aufgehört zu angeln." } })
 end
 
 -- Hauptthread
@@ -57,11 +48,13 @@ Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
 
+        -- Überprüfen, ob der Spieler in der Nähe eines Angelspots ist
         local isNear, spot = isNearFishingSpot()
 
         if isNear and not isFishing then
             DrawText3D(spot.x, spot.y, spot.z, "Drücke [E], um zu angeln")
 
+            -- Wenn der Spieler E drückt, startet oder stoppt das Angeln
             if IsControlJustPressed(1, Config.FishingKey) then
                 startFishing()
             end
@@ -96,20 +89,3 @@ function DrawText3D(x, y, z, text)
         DrawText(_x, _y)
     end
 end
--- Überprüfe, ob der Spieler beim Verkäufer ist und Fische verkaufen möchte
-Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
-
-        local playerCoords = GetEntityCoords(PlayerPedId())
-        local distance = #(playerCoords - vector3(Config.Seller.x, Config.Seller.y, Config.Seller.z))
-
-        if distance < 3.0 then
-            DrawText3D(Config.Seller.x, Config.Seller.y, Config.Seller.z, "Drücke [E], um Fische zu verkaufen")
-
-            if IsControlJustPressed(1, Config.FishingKey) then
-                TriggerServerEvent('fishing:sell')
-            end
-        end
-    end
-end)
